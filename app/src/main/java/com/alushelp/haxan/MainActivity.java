6b7ddda66ab3e347a246055919e6a0968368fe80
armutaqin <adityarahmatmutaqin@gmail.com>
2018-11-12 13:13:10 +0700

    add bottom navigation bar

--- app/src/main/java/com/alushelp/haxan/MainActivity.java
+++ app/src/main/java/com/alushelp/haxan/MainActivity.java
@@ -1,7 +1,11 @@
 package com.alushelp.haxan;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_nav_main);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment = null;
+                        switch (item.getItemId()){
+                            case R.id.action_home:
+                                fragment = new HomeFragment();
+                                break;
+                            case R.id.action_pesanan:
+                                fragment = new PesananFragment();
+                                break;
+                            case R.id.action_lainnya:
+                                fragment = new LainnyaFragment();
+                                break;
+                        }
+                        return loadFragment(fragment);
+                    }
+                }
+        );
+
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container_main, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
